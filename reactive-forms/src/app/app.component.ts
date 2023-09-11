20710f23b8cd3f0f8ee970a5d804eaa23733f8e3
Akash Yadav <Akash.Yadav@asl.mum.sst>
2023-09-11 23:12:58 +0530

    added

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -1,45 +1,45 @@
-import { Component, OnInit } from '@angular/core'; 
-import { FormGroup, FormControl, Validators } from '@angular/forms'; 
- 
-@Component({ 
-  selector: 'app-root', 
-  templateUrl: './app.component.html', 
-  styleUrls: ['./app.component.css'] 
-}) 
-export class AppComponent implements OnInit { 
-  onSubmit() { 
-    
-    console.log(this.userForm); 
-  } 
-  resetForm() { 
-    this.userForm.reset 
-  } 
- 
-  title = 'reactive-forms'; 
-  genders: string[] = ['male', 'female']; 
-  userForm!: FormGroup 
-  invalidusername: string[] = ['admin ']; 
-  ngOnInit(): void { 
-    this.userForm = new FormGroup({ 
-      'username': new FormControl(null, [Validators.required, Validators.minLength(4), 
-        Validators.pattern(/^[a-zA-Z0-9]*$/),this.invalidNames.bind(this)]), 
-      'email': new FormControl(null, [Validators.required, Validators.email]), 
-      'number': new FormControl(null, Validators.required), 
-      'gender': new FormControl('male', Validators.required) 
- 
- 
-    }) 
- 
- 
-  } 
-  invalidNames(control:FormControl):{[s:string]:boolean}{ 
-    if(this.invalidusername.includes(control.value)){ 
-      return {'invalidNames':true} 
-    } 
-    return {'invalidNames':false};
-  } 
+import { Component, OnInit} from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'reacted-forms';
+  genderList:string[]=['Male','Female']
+  genders:string[]=['Male','Female']
+  
+  userForm!:FormGroup;
+  invalidUserNames=['admin','users']
+
+  ngOnInit():void{
+    this.userForm = new FormGroup({
+      'username': new FormControl(null, [Validators.required, 
+        Validators.minLength(5), Validators.maxLength(15),
+        Validators.pattern("^[a-zA-Z ]*$"),
+        this.inValidNames.bind(this)]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'number': new FormControl('', Validators.required),
+      'gender': new FormControl('Male', Validators.required)
+    })
+  }
+
+  onSubmit(){
+  
+    console.log(this.userForm);
+  }
+
+  inValidNames(control:FormControl):{[s:string]:boolean}{
+    if(this.invalidUserNames.includes(control.value)){
+      return {'invalidName':true};
+    }
+    return {}
+  }
 
   onClickReset()
 {
   this.userForm.reset();
-}}
+}
+}
